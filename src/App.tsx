2de8c9092d36dbfbe8f3b05627acf82c46405201
Shantanu Pramanik <srs.shaan@gmail.com>
2024-05-09 11:52:32 +0530

    Added Tailwind CSS dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 function App() {
-	return (
-		<>
-			<h1>Site Under Development by Shantanu.</h1>
-		</>
-	);
+  return (
+    <>
+      <div className="place-content-center">
+        <h1 className="text font-bold underline text-4xl">Hello World !</h1>
+        <h1>Site Under Development by Shantanu.</h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
