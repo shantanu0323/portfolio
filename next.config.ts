136331bd1326e01257f533bfaffc6150696b7951
Shantanu Pramanik <shantanu@needle-ai.com>
2025-05-06 15:12:34 +0200

    [Added] Deploy configurations

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+    /* config options here */
+    reactStrictMode: true,
+    images: {
+        unoptimized: true,
+    },
+    basePath: process.env.BASE_PATH || '',
+    assetPrefix: process.env.BASE_PATH || '',
 };
 
 export default nextConfig;
